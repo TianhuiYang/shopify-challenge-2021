17fab021fba094df3534e249d3d2a167789a50f8
Tianhui Yang <tianhui.yg@gmail.com>
2021-05-03 14:41:53 -0400

    feat: API service

---
+++ src/service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL = "http://www.omdbapi.com/";
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const getMovieByTitle = async (query) => {
+  return axios
+    .get(`${BASE_URL}?s=${query}&type=movie&apikey=${API_KEY}`)
+    .then((res) => {
+      const movies = res.data.Search;
+      return !!movies ? movies : [];
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export const getMovieById = async (id) => {
+    return axios
+    .get(`${BASE_URL}?i=${id}&type=movie&apikey=${API_KEY}`)
+    .then((res) => {
+      const movies = res.data.Search;
+      return !!movies ? movies : [];
+    })
+    .catch((err) => {
+      return err;
+    });
+};
